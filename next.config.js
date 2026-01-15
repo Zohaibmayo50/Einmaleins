c2d4fe382350b6a5c6d4c271c436481620565441
Zohaibmayo50 <zohaibmayo50@users.noreply.github.com>
2026-01-16 01:06:09 +0500

    Rename tabla to einmaleins for German URLs

--- next.config.js
+++ next.config.js
@@ -2,25 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
-  async redirects() {
-    return [
-      {
-        source: '/para-estudiantes',
-        destination: '/fuer-schueler',
-        permanent: true,
-      },
-      {
-        source: '/para-padres',
-        destination: '/fuer-eltern',
-        permanent: true,
-      },
-      {
-        source: '/para-profesores',
-        destination: '/fuer-lehrer',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
